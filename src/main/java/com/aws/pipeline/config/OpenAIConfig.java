0addcbbd264265de6250ddf9765b9f846a612a97
mitvaghasiya <mit.v@brilworkss.com>
2023-12-13 16:20:32 +0530

    first commit

--- src/main/java/com/aws/pipeline/config/OpenAIConfig.java
+++ src/main/java/com/aws/pipeline/config/OpenAIConfig.java
@@ -6,23 +6,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 
 @Configuration
 public class OpenAIConfig implements WebMvcConfigurer {
 
     @Value("${openai.api.key}")
-     String openaiApiKey;
+    private String openaiApiKey;
 
-    @Bean
-    public RestTemplate template(){
-        RestTemplate restTemplate=new RestTemplate();
-        restTemplate.getInterceptors().add((request, body, execution) -> {
+  /*  @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.interceptors((request, body, execution) -> {
             request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
             return execution.execute(request, body);
-        });
-        return restTemplate;
+        }).build();
     }
-
+*/
+  @Bean
+  public RestTemplate template() {
+      RestTemplate restTemplate = new RestTemplate();
+      restTemplate.getInterceptors().add((request, body, execution) -> {
+          request.getHeaders().add("Authorization", "Bearer " + openaiApiKey);
+          System.out.println("Request URL: " + request.getURI());
+          System.out.println("Request Headers: " + request.getHeaders());
+          System.out.println("Request Body: " + new String(body));
+          return execution.execute(request, body);
+      });
+      return restTemplate;
+  }
 
 
     @Override
@@ -32,9 +43,4 @@ public class OpenAIConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST");
         System.out.println("CORS configuration loaded.");
     }
-
-
-
-
 }
-
--- src/main/java/com/aws/pipeline/controller/CustomBotController.java
+++ src/main/java/com/aws/pipeline/controller/CustomBotController.java
@@ -5,6 +5,7 @@ import com.aws.pipeline.dto.ChatGptResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -21,12 +22,31 @@ public class CustomBotController {
     @Autowired
     private RestTemplate template;
 
-    @GetMapping("/chat")
+  /*  @GetMapping("/chat")
     public String chat(@RequestParam("prompt") String prompt){
         ChatGPTRequest request=new ChatGPTRequest(model, prompt);
         ChatGptResponse chatGptResponse = template.postForObject(apiURL, request, ChatGptResponse.class);
         return chatGptResponse.getChoices().get(0).getMessage().getContent();
+    }*/
+
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("prompt") String prompt){
+        try {
+            ChatGPTRequest request = new ChatGPTRequest(model, prompt);
+            ChatGptResponse chatGptResponse = template.postForObject(apiURL, request, ChatGptResponse.class);
+            return chatGptResponse.getChoices().get(0).getMessage().getContent();
+        } catch (HttpClientErrorException.Unauthorized e) {
+            // Log or handle the Unauthorized exception
+            e.printStackTrace();
+            return "Unauthorized: " + e.getMessage();
+        } catch (Exception e) {
+            // Handle other exceptions
+            e.printStackTrace();
+            return "Error: " + e.getMessage();
+        }
     }
+
 }
 
 
